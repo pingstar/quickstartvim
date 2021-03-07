22a7629abecf8bb49b1235e46c8bb272693d5c0e
kubeshared <markhub.cn@gmail.com>
2021-03-08 00:27:50 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Install go-langserver..."
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
+go get -u github.com/sourcegraph/go-langserver
+
+
+if [ ! -d "$HOME/.SpaceVim.d" ]; then
+  echo "Not install spacevim, now install..."
+  curl -sLf https://spacevim.org/cn/install.sh | bash
+
+else
+
+  echo "spacevim already installed"
+  
+fi
+
+echo "Configuration spacevim"
+
+cp ./sublimemonokai.vim $HOME/.SpaceVim/colors/
+cp -r ./spacevim.d $HOME/.SpaceVim.d
